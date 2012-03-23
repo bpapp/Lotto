d66e06aa1cd696c2b7c2ac99304d5b85eeb14141
bpapp <belu.papp@gmail.com>
2012-03-23 22:58:35 +0100

    teljesen mas konstrukcio

---
+++ Lottery/test/lottery/LotteryJUnitTest2.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lottery;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Papp Béla
+ */
+public class LotteryJUnitTest2 {
+
+    public LotteryJUnitTest2() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAnException() {
+        List<Integer> list = Lottery.doProduceRandomNumbers();
+
+        for (int i = 0, j = 1; i < list.size() - 1; i++, j++) {
+            if (list.get(i) > list.get(j) || list.get(i) == list.get(j)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
